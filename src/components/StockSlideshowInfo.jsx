aaae181e54c230d50f32bd9c5445b837d03037bf
Rishabh kumar <imrishabh9628@gmail.com>
2023-05-24 23:45:46 +0530

    added stock price slideshow

--- src/components/StockSlideshowInfo.jsx
+++ src/components/StockSlideshowInfo.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from 'react';
+import './StockSlideshowInfo.css';
 
-const StockSlideshowInfo = () => {
+const StockSlideshowInfo = ({ StockName, StockPrice, ChangePercentage }) => {
+    
+  const ArrowLogo = ChangePercentage > 0 ? (
+  <img src="/images/Green-Arrow.png" alt="Up Arrow" />
+) : (
+  <img src="/images/Red-Arrow.png" alt="Down Arrow" />
+);
+
   return (
-    <div>
-      
+    <div className="StockSlideshowInfoBox">
+      <div className="stock-name-box">
+        <div className="stock-name">{StockName}</div>
+      </div>
+      <div className="stock-data-box">
+        <div className="stock-price">{StockPrice}</div>
+        <div className="stock-logo">{ArrowLogo}</div>
+        <div className="stock-percentage-change">{ChangePercentage}%</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default StockSlideshowInfo
+export default StockSlideshowInfo;
