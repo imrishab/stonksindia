1555a74ebbb30d5495894e4783378df2989dd83d
Rishabh kumar <imrishabh9628@gmail.com>
2023-05-27 18:02:53 +0530

    style - added stock screener cards on homepage

--- src/components/StockInfo.jsx
+++ src/components/StockInfo.jsx
@@ -1,24 +1,32 @@
 import React from 'react'
 import './StockInfo.css'
 
-const StockInfo = ({ StockName, StockTicker, ChangePercentage }) => {
-
-    const ArrowLogo = ChangePercentage > 0 ? (
-        <img src="/images/Green-Arrow.png" alt="Up Arrow" />
+const StockInfo = ({ StockName, StockTicker, ChangePercentage, style, NameStyle, TickerStyle, PercentageChangeStyle }) => {
+    const isPositiveChange = ChangePercentage > 0;
+    
+    const ArrowLogo = isPositiveChange ? (
+      <img src="/images/Green-Arrow.png" alt="Up Arrow" />
     ) : (
-        <img src="/images/Red-Arrow.png" alt="Down Arrow" />
+      <img src="/images/Red-Arrow.png" alt="Down Arrow" />
     );
-
+  
+    const percentageChangeColor = isPositiveChange ? 'rgb(0,185,7)' : 'rgb(185,0,0)';
+  
     return (
-
-        <div className="StockInfoBox">
-            <div className="stock-name">{StockName}</div>
-            <div className="stock-ticker">{StockTicker}</div>
-            <div className="stock-logo">{ArrowLogo}</div>
-            <div className="stock-percentage-change">{ChangePercentage}%</div>
+      <div className="StockInfoBox" style={style}>
+        <div className="stock-name" style={NameStyle}>
+          {StockName}
         </div>
-
-    )
-}
+        <div className="stock-ticker" style={TickerStyle}>
+          {StockTicker}
+        </div>
+        <div className="stock-logo">{ArrowLogo}</div>
+        <div className="stock-percentage-change" style={{ ...PercentageChangeStyle, color: percentageChangeColor }}>
+          {ChangePercentage}%
+        </div>
+      </div>
+    );
+  };
+  
 
 export default StockInfo
