f651bafc13f2e74e54b4997bad3be556ee0061d1
Rishabh kumar <imrishabh9628@gmail.com>
2023-05-29 11:54:29 +0530

    feat - added live stock price in slideshow

--- src/components/StockSlideshowInfo.jsx
+++ src/components/StockSlideshowInfo.jsx
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './StockSlideshowInfo.css';
 
-const StockSlideshowInfo = ({ StockName, StockPrice, ChangePercentage }) => {
-    
-  const ArrowLogo = ChangePercentage > 0 ? (
+const StockSlideshowInfo = ({ nseCode }) => {
+  const [stockData, setStockData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://api.stockmarketapi.in/api/v1/getprices', {
+          params: {
+            token: process.env.REACT_APP_API_KEY,
+            nsecode: nseCode
+          }
+        });
+        const data = response.data.data[nseCode];
+        console.log('Fetched data:', data);
+        setStockData(data);
+      } catch (error) {
+        console.error('Error fetching stock data:', error);
+      }
+    };
+
+    fetchData();
+  }, [nseCode]);
+
+  if (!stockData) {
+    return <div>Loading stock data...</div>;
+  }
+
+
+  const ArrowLogo = stockData.dayChangePerc > 0 ? (
   <img src="/images/Green-Arrow.png" alt="Up Arrow" />
 ) : (
   <img src="/images/Red-Arrow.png" alt="Down Arrow" />
@@ -12,15 +39,26 @@ const StockSlideshowInfo = ({ StockName, StockPrice, ChangePercentage }) => {
   return (
     <div className="StockSlideshowInfoBox">
       <div className="stock-name-box">
-        <div className="stock-name">{StockName}</div>
+        <div className="stock-name">{stockData.NSECode}</div>
       </div>
       <div className="stock-data-box">
-        <div className="stock-price">{StockPrice}</div>
+        <div className="stock-price">{stockData.ltp}</div>
         <div className="stock-logo">{ArrowLogo}</div>
-        <div className="stock-percentage-change">{ChangePercentage}%</div>
+        <div className="stock-percentage-change">{stockData.dayChangePerc}%</div>
       </div>
     </div>
   );
 };
 
 export default StockSlideshowInfo;
+
+
+
+
+
+
+
+
+
+
+
